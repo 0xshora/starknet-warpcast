19653b71454b3a8c7aea5ee03eb94753d736e3b0
0xshora <112358132134.fibon@gmail.com>
2024-03-16 16:17:46 +0000

    fix: url

--- frames/map-fid-starknet-address/src/pages/index.tsx
+++ frames/map-fid-starknet-address/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="Frame" />
-        <meta property="og:image" content={`${BASE_URL}/starknet.png`} />
+        <meta property="og:image" content={`https://starknet-warpcast.vercel.app/starknet.png`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${BASE_URL}/starknet.png`} />
         <meta property="fc:frame:button:1" content="Connect Starknet Wallet" />
