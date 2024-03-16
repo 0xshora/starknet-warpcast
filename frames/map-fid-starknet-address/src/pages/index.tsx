c21b578a954c6a033a7c974af170517afcaf0b41
0xshora <112358132134.fibon@gmail.com>
2024-03-16 16:46:27 +0000

    fix: url

--- frames/map-fid-starknet-address/src/pages/index.tsx
+++ frames/map-fid-starknet-address/src/pages/index.tsx
@@ -1,20 +1,26 @@
 import { BASE_URL } from "@/utils";
 import Head from "next/head";
 
+
+
 export default function Home() {
   return (
     <>
       <Head>
         <meta property="og:title" content="Frame" />
-        <meta property="og:image" content={`https://starknet-warpcast.vercel.app/starknet.png`} />
+        <meta property="og:image" content={`https://starknet-warpcast-git-main-syora17s-projects.vercel.app/starknet.png`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${BASE_URL}/starknet.png`} />
-        <meta property="fc:frame:button:1" content="Connect Starknet Wallet" />
-        <meta property="fc:frame:button:1:action" content="post_redirect" />
+        {/* <meta property="fc:frame:button:1" content="Connect Starknet Wallet" />
+        <meta property="fc:frame:button:1:action" content="post_redirect" /> 
         <meta
           property="fc:frame:post_url"
           content={`${BASE_URL}api/redirect`}
-        />
+        /> */}
+        <meta property="fc:frame:button:1" content="Green" />
+        <meta property="fc:frame:button:2" content="Purple" />
+        <meta property="fc:frame:button:3" content="Red" />
+        <meta property="fc:frame:button:4" content="Blue" />
       </Head>
     </>
   );
